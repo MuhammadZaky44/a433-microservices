9f5b3491d9ad723b7595f6514d72c40b06679088
Asep <zakyfatih44@gmail.com>
2023-08-06 19:46:38 +0700

    Added build_push_image_karsajobs-ui.sh file

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,11 @@
+# Membuat docker image dari dockerfile dengan nama item-app dan tag:v1
+docker build -t zakyfatih/karsajobs-ui:latest
+
+# Mengubah nama image yang sudah dibuat menjadi sesuai nama repository Docker Hub
+# docker tag karsajobs:latest zakyfatih/karsajobs:latest
+
+# Melakukan login ke Docker Hub dengan menggunakan cmd variable PASSWORD_DOCKER_HUB
+echo $PASSWORD_DOCKER_HUB | docker login -u zakyfatih --password-stdin
+
+# Mempush image ke Docker Hub
+docker push zakyfatih/karsajobs-ui:latest
