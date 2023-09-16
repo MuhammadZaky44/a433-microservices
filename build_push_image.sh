03e700f3b7371ab5dd086c18c58fb8c1989fb9b6
Asep <zakyfatih44@gmail.com>
2023-09-16 13:46:04 +0700

    Now able to push to GHCR

--- build_push_image.sh
+++ build_push_image.sh
@@ -3,3 +3,9 @@ docker build -t zakyfatih/order-service:latest .
 
 # Mempush hasil build image ke Docker-hub repository
 docker push zakyfatih/order-service:latest
+
+# Mengubah tag dari image untuk Dockerhub menjadi Github Container Registry
+docker tag zakyfatih/order-service:latest ghcr.io/muhammadzaky44/order-service:latest
+
+# Mempush image ke Github Container Registry
+docker push ghcr.io/muhammadzaky44/order-service:latest
